17e414ef148dbbba1c382352aaab11003f295c96
jwwallin <jussi.w.wallin@gmail.com>
2020-01-31 18:42:39 +0200

    refactor

--- metadata-report/metadata-report.py
+++ metadata-report/metadata-report.py
@@ -13,12 +13,8 @@ output_file.write(f'Report for {os.path.abspath(input_directory)}\n\n')
 for entry in os.listdir(input_directory):
   file_path = os.path.join(input_directory, entry)
   if os.path.isfile(file_path):
-    if filetype.guess(file_path) is not None:
-      files.append(entry)
-
-for file in files:
-  output_file.write(f'{file}:\n')
-  file_type = mutagen.File(os.path.join(input_directory, file))
-  if file_type is not None:
-    output_file.write(file_type.pprint())
-    output_file.write('\n\n')
+    file_type = mutagen.File(file_path)
+    if file_type is not None:
+      output_file.write(f'{entry}:\n')
+      output_file.write(file_type.pprint())
+      output_file.write('\n\n')
